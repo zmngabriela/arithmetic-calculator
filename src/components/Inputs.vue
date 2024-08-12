2b5229a445a1c686b7730591c64619673ef4c2bd
Gabriela Zimmermann <zmngabriela@gmail.com>
2024-08-12 18:05:59 +0200

    fixing ux functionality

--- src/components/Inputs.vue
+++ src/components/Inputs.vue
@@ -4,7 +4,7 @@ const props = defineProps(['isCalculated', 'result', 'getNum1', 'getNum2', 'num1
 
 <template>
     <div class="inputs centered">
-        <input :value="num1" @change="props.getNum1" type="number" placeholder="Number">
-        <input :value="num2" @change="props.getNum2" type="number" placeholder="Number">
+        <input :value="num1" @change="getNum1" type="number" placeholder="Number">
+        <input :value="num2" @change="getNum2" type="number" placeholder="Number">
     </div>
 </template>
